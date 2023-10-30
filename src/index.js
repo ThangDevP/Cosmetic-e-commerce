03f9fca9bb8a1d3596ecf34c5abe82ca0214f4cb
ThangDevP <thang.nguyen-ojt07@devplus.edu.vn>
2023-10-30 16:51:10 +0700

    feat/EEW-1: Create code base public

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const morgan = require('morgan')
+// const hbs = require('express-handlebars');
+// var app = express.createServer();
+const router = express.Router();
+const path = require('path');
+const app = express()
+const port = 8000
+
+// Show request, HTTP logger
+app.use(morgan('combined'))
+
+// Use static file
+app.use(express.static(path.join(__dirname, 'public')));
+
+// 
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname + '/public/views/html/home.html'));
+  //__dirname : It will resolve to your project folder.
+});
+app.use('/', router);
+// Local host --- Hosting
+// Action ---> Dispatcher ---> Function handler
+app.listen(port, () => {
+  console.log(`Ecommerce website listening on http://localhost:${port}`)
+})
