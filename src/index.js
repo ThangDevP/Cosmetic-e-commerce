e1d9d3571a6379a155243d321f25b7cc0cd29bb1
Sondtgcd191140 <son.do-ojt07@devplus.edu.vn>
2023-11-06 17:03:10 +0700

    feat/EEW-12: Add cart

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
-const express = require('express');
-const morgan = require('morgan');
-const path = require('path');
-const cors = require('cors');
-const jsonServer = require('json-server');
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const cors = require("cors");
+const jsonServer = require("json-server");
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const app = express();
 const port = 3000;
-// Set up CORS for your Express app
+const cart = [];
+
 app.use(cors());
-// Show request, HTTP logger
-app.use(morgan('combined'));
-// Use static file
-app.use(express.static(path.join(__dirname, 'styles')));
-app.use(express.static(path.join(__dirname, 'scripts')));
-app.use(express.static(path.join(__dirname, 'components')));
-app.use(express.static(path.join(__dirname, 'pages')));
-app.use(express.static(path.join(__dirname, 'assets/img')));
+app.use(morgan("combined"));
+app.use(express.static(path.join(__dirname, "styles")));
+app.use(express.static(path.join(__dirname, "scripts")));
+app.use(express.static(path.join(__dirname, "components")));
+app.use(express.static(path.join(__dirname, "pages")));
+app.use(express.static(path.join(__dirname, "assets/img")));
 
 // JSON Server setup
 const server = jsonServer.create();
@@ -30,7 +29,6 @@ server.use(
 );
 server.use(jsonServer.bodyParser);
 
-
 server.use((req, res, next) => {
   if (req.method === "POST") {
     req.body.createdAt = Date.now();
@@ -41,36 +39,207 @@ server.use((req, res, next) => {
   next();
 });
 
-server.post('/register', (req, res) => {
+// router.post("/api/cart", (req, res) => {
+//   try {
+//     const productData = req.body;
+//     /// Kiểm tra xem sản phẩm có tồn tại trong giỏ hàng chưa
+//   const existingProductIndex = cart.findIndex((item) => item.id === productData.id);
+
+//   if (existingProductIndex !== -1) {
+//     // Nếu sản phẩm đã tồn tại, cập nhật số lượng
+//     cart[existingProductIndex].quantity += productData.quantity;
+//   } else {
+//     // Nếu sản phẩm chưa tồn tại, thêm mới vào giỏ hàng
+//     cart.push(productData);
+//   }
+//     res.status(200).json({ message: "Product added to cart successfully." });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: "Internal Server Error" });
+//   }
+// });
+router.post("/api/cart", (req, res) => {
+  try {
+    const productData = req.body;
+    // Check if the product already exists in the cart
+    const existingProductIndex = cart.findIndex((item) => item.id === productData.id);
+
+    if (existingProductIndex !== -1) {
+      // If the product already exists, update the quantity
+      cart[existingProductIndex].quantity += productData.quantity;
+      res.status(200).json({ message: "Product quantity updated in the cart successfully." });
+    } else {
+      // If the product doesn't exist, add it to the cart
+      cart.push(productData);
+      res.status(200).json({ message: "Product added to the cart successfully." });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+
+const fs = require('fs');
+
+const cartDataFile = 'db.json'; 
+
+// API thêm sản phẩm vào giỏ hàng
+router.post('/cart', (req, res) => {
+
+  // Đọc dữ liệu giỏ hàng hiện tại
+  fs.readFile(cartDataFile, (err, data) => {
+    if (err) throw err;
+    
+    let cart = JSON.parse(data);
+
+    const newProduct = req.body; // sản phẩm cần thêm mới
+    
+    // Kiểm tra sản phẩm đã tồn tại chưa
+    const existingProduct = cart.find(p => p.id == newProduct.id);
+
+    if (existingProduct) {
+      // Nếu sp đã có, cộng dồn số lượng
+      existingProduct.quantity += newProduct.quantity;
+    } else {
+      // Nếu sp chưa có, thêm mới vào mảng
+      cart.push(newProduct); 
+    }
+
+    // Ghi đè giỏ hàng vào file
+    fs.writeFile(cartDataFile, JSON.stringify(cart), err => {
+      if (err) throw err;
+      res.json({message: 'Thêm sản phẩm thành công!'});
+    });
+
+  });
+
+});
+
+
+// API cập nhật số lượng sản phẩm trong giỏ
+router.patch('/cart/:productId', (req, res) => {
+  
+  const productId = req.params.productId;
+  const newQuantity = req.body.quantity;
+
+  fs.readFile(cartDataFile, (err, data) => {
+    if (err) throw err;
+
+    let cart = JSON.parse(data);
+
+    // Tìm sản phẩm cần update
+    const product = cart.find(p => p.id == productId);
+
+    if (product) {
+      product.quantity = newQuantity; // Cập nhật số lượng
+      
+      fs.writeFile(cartDataFile, JSON.stringify(cart), err => {
+        if (err) throw err;
+        res.json({message: 'Cập nhật số lượng thành công!'})  
+      });
+
+    } else {
+      res.status(404).json({message: 'Không tìm thấy sản phẩm!'});
+    }
+
+  });
+
+});
+
+
+// API xóa sản phẩm khỏi giỏ hàng
+router.delete('/cart/:productId', (req, res) => {
+
+  const productId = req.params.productId;
+
+  fs.readFile(cartDataFile, (err, data) => {
+    if (err) throw err;
+
+    let cart = JSON.parse(data);
+
+    // Tìm và xóa sản phẩm khỏi mảng
+    const productIndex = cart.findIndex(p => p.id == productId);
+    if (productIndex >= 0) {
+      cart.splice(productIndex, 1); 
+    }
+
+    fs.writeFile(cartDataFile, JSON.stringify(cart), err => {
+      if (err) throw err;  
+      res.json({message: 'Đã xóa sản phẩm khỏi giỏ hàng!'}) ;
+    });
+
+  });
+
+
+});
+
+
+app.get("/cart/:id", (req, res) => {
+  const productId = req.params.id;
+
+  // Tìm sản phẩm trong giỏ hàng dựa trên productId (hoặc product-id, tùy vào cấu trúc của dữ liệu cart)
+  // Ví dụ: sử dụng productId để tìm sản phẩm trong dữ liệu cart
+  const productInCart = cart.find((item) => item["product-id"] === productId);
+
+  if (productInCart) {
+    // Nếu tìm thấy sản phẩm trong giỏ hàng, bạn có thể hiển thị thông tin sản phẩm
+    res.send(`Product Name: ${productInCart.name}, Quantity: ${productInCart.quantity}`);
+  } else {
+    // Nếu không tìm thấy sản phẩm, bạn có thể trả về một trang hoặc thông báo lỗi
+    res.send("Product not found in the cart.");
+  }
+});
+
+
+server.post("/register", (req, res) => {
   const { username, password } = req.body;
   // Check if the user already exists
-  const existingUser = router.db.get('users').find({ username }).value();
+  const existingUser = router.db.get("users").find({ username }).value();
   if (existingUser) {
-    return res.status(400).json({ message: 'Username already exists.' });
+    return res.status(400).json({ message: "Username already exists." });
   }
   // If the user doesn't exist, add them to the database
   const id = Date.now();
   const newUser = { id, username, password };
-  router.db.get('users').push(newUser).write();
-  return res.status(200).json({ message: 'Registration successful.' });
+  router.db.get("users").push(newUser).write();
+  return res.status(200).json({ message: "Registration successful." });
 });
 
 // Mount the JSON Server on the '/api' path
-app.use('/api', server);
+app.use("/api", server);
 
 // Serve your routes
-app.get('/about-us', function (req, res) {
-  res.sendFile(path.join(__dirname + '/pages/about-us.html'));
+app.get("/about-us", function (req, res) {
+  res.sendFile(path.join(__dirname + "/pages/about-us.html"));
 });
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/pages/main.html'));
+app.get("/main", function (req, res) {
+  res.sendFile(path.join(__dirname + "/pages/main.html"));
 });
-app.get('/login', function (req, res) {
-  res.sendFile(path.join(__dirname + '/pages/login.html'));
+app.get("/login", function (req, res) {
+  res.sendFile(path.join(__dirname + "/pages/login.html"));
 });
-app.get('/product', function (req, res) {
-  res.sendFile(path.join(__dirname + '/pages/product.html'));
+app.get("/cart", function (req, res) {
+  res.sendFile(path.join(__dirname + "/pages/cart.html"));
 });
+
+app.get("/product/:id", function (req, res) {
+  const productId = Number(req.params.id); // Use req.params.id to access the route parameter
+  console.log(productId);
+  fetch(`http://localhost:3000/api/products?id=${productId}`)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.length === 1) {
+        res.sendFile(path.join(__dirname + "/pages/product.html"));
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching product data: ", error);
+      // You may want to send an error response here
+    });
+});
+
+module.exports = router;
 server.use(router);
 // Local host --- Hosting
 app.listen(port, () => {
--- src/scripts/product.js
+++ src/scripts/product.js
@@ -1,5 +1,60 @@
-// Assuming you have a product ID (e.g., 1) you want to display
-const productId = 1; // Update this with the desired product ID
+const currentURL = window.location.href;
+const segments = currentURL.split('/');
+const productId = segments[segments.length - 1];
+
+document.getElementById("addToCart").addEventListener("click", () => {
+  const productId = segments[segments.length - 1];
+  const productName = document.getElementById("product-name").textContent;
+  const productPrice = parseFloat(document.getElementById("product-price").textContent);
+  const productQuantity = parseInt(document.getElementById("quantity").textContent, 10);
+
+  // Tạo dữ liệu sản phẩm để thêm vào giỏ hàng
+  const productData = {
+    id: productId,
+    name: productName,
+    price: productPrice,
+    quantity: productQuantity,
+  };
+
+  fetch("/api/cart")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Lỗi API");
+    }
+    return response.json();
+  })
+  .then((cartItems) => {
+    // Check if the product already exists in the cart
+    const existingProductIndex = cartItems.findIndex((item) => item.id === productData.id);
+
+    if (existingProductIndex !== -1) {
+      // If the product already exists, update its quantity
+      cartItems[existingProductIndex].quantity += productData.quantity;
+    } else {
+      // If the product is not in the cart, add it
+      cartItems.push(productData);
+    }
+
+    // Perform a POST request to update the cart on the server
+    return fetch("/api/cart", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(cartItems), // Send the entire cartItems array
+    });
+  })
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Error adding product to cart");
+    }
+    // Xử lý khi sản phẩm đã được thêm vào giỏ hàng
+    alert("Sản phẩm đã được thêm vào giỏ hàng");
+    window.location.href = "/cart";
+  })
+  .catch((error) => console.error("Error adding product to cart: ", error));
+});
+
 
 fetch(`/api/products/${productId}`) // Fetch the product data by its ID
   .then((response) => response.json())
@@ -7,12 +62,13 @@ fetch(`/api/products/${productId}`) // Fetch the product data by its ID
     // Update the HTML elements with the product details
     document.getElementById("product-name").textContent = product.name;
     document.getElementById("product-price").textContent = product.price;
-    document.getElementById("product-price-discount").textContent =
-      (product.price * (100 - product.discount)) / 100;
+    const finalPrice = (document.getElementById(
+      "product-price-discount"
+    ).textContent = (product.price * (100 - product.discount)) / 100);
     document.getElementById("product-description").textContent =
       product.description;
     document.getElementById("product-category").textContent = product.category;
-    document.getElementById("product-image").src = product.img;
+    document.getElementById("product-image").src = "/" + product.img;
 
     // Calculate the total price based on the quantity and update it
     const decrementButton = document.querySelector(".decrement");
@@ -22,7 +78,7 @@ fetch(`/api/products/${productId}`) // Fetch the product data by its ID
 
     function updateTotalPrice() {
       const quantity = parseInt(quantityLabel.textContent, 10);
-      const total = product.price * quantity;
+      const total = finalPrice * quantity;
       totalPriceLabel.textContent = total;
     }
 
