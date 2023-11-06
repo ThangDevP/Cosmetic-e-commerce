974fc8aa6f01f90f15b1f3de11f59aa20ca5bf01
tvpkhangojt07 <khang.truong-ojt07@devplus.edu.vn>
2023-11-06 09:27:34 +0700

    feat/EEW-26: add :id into user

--- src/scripts/user.js
+++ src/scripts/user.js
@@ -1,35 +1,19 @@
-function fetchUserData() {
-  fetch("http://localhost:3000/api/users")
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      // Assuming you want to display the first user's information
-      const user = data[0];
+const currentURL = window.location.href;
+const segments = currentURL.split("/");
+const userId = segments[segments.length - 1];
 
-      if (user) {
-        displayUserInfo(user);
-      } else {
-        console.error("User not found in the response.");
-      }
-    })
-    .catch((error) => {
-      console.error("Error fetching user data:", error);
-    });
-}
+fetch(`http://localhost:3000/api/users/${userId}`)
+  .then((response) => response.json())
+  .then((user) => {
+    console.log(user);
+    const fullNameElement = document.getElementById("full-name");
+    const emailElement = document.getElementById("email");
+    const passwordElement = document.getElementById("password");
 
-function displayUserInfo(user) {
-  const fullNameElement = document.getElementById("full-name");
-  const emailElement = document.getElementById("email");
-  const passwordElement = document.getElementById("password");
-
-  // Update the HTML elements with user information
-  fullNameElement.textContent = `Full Name: ${user.username}`;
-  emailElement.textContent = `Email: ${user.username}`;
-  passwordElement.textContent = `Password: ${user.password}`;
-}
-
-fetchUserData();
+    fullNameElement.textContent = `Full Name: ${user.username}`;
+    emailElement.textContent = `Email: ${user.username}`;
+    passwordElement.textContent = `Password: ${user.password}`;
+  })
+  .catch((error) => {
+    console.error("Error fetching user data:", error);
+  });
