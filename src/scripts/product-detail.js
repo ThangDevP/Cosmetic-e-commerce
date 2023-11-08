8908bd33948353fe1df8f898b514772d7b014b37
tvpkhangojt07 <khang.truong-ojt07@devplus.edu.vn>
2023-11-08 09:54:10 +0700

    feat/EEW-28: edit product detail page

--- src/scripts/product-detail.js
+++ src/scripts/product-detail.js
@@ -8,13 +8,17 @@ fetch(`/api/products/${productId}`)
   .then((product) => {
     // Update the HTML elements with the product details
     document.querySelector(".product-title").textContent = product.name;
-    document.querySelector(".product-price").textContent = product.price + " đ";
-    // document.querySelector(".product-price-discount").textContent =
-    //   (product.price * (100 - product.discount)) / 100;
+    document.querySelector(".product-price-basic").textContent = product.price + " đ";
+    if (product.discount !== 0) {
+    document.querySelector(".product-price-discount").textContent =
+      (product.price * (100 - product.discount)) / 100 + " đ";
+    } else {
+      document.querySelector(".product-price-discount").textContent = "";
+      document.querySelector(".product-price-basic").style.textDecoration = 'none';
+    }
+
     document.querySelector(".product-description").textContent = product.description;
-    // Assuming you have an element with class "product-category"
-    // document.querySelector(".product-category").textContent = product.category;
-    // Assuming you have an element with class "product-image"
+    document.querySelector(".product-category").textContent = product.category;
     document.querySelector(".img").src = "/" + product.img;
   })
   .catch((error) => console.error("Error fetching product data: ", error));
