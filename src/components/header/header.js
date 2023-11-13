4a69745c20afb0fca4a58031412aabd6bb9ea14e
nchungojt07 <chung.nguyen-ojt07@devplus.edu.vn>
2023-11-13 09:01:23 +0700

    feat/EEW-5: fix user login with name in header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -8,55 +8,31 @@ function dropdownFunc() {
     x.style.display = "block";
   }
 }
-function createSubMenu() {
-  var userChange = document.getElementById('user-change');
-  var subMenu = document.createElement('ul');
-  subMenu.classList.add('sub-menu'); // You can add CSS classes for styling
-  subMenu.innerHTML = `
-    <li><a href="/profile">User Profile</a></li>
-    <li><a href="#" id="logout">Logout</a></li>
-  `;
-
-  userChange.appendChild(subMenu);
-  
-
-  // Add event listener to hide the sub-menu when the cursor leaves "user-change"
-  userChange.addEventListener('mouseleave', function () {
-    subMenu.style.display = 'none';
-  });
-
-  // Add event listener to show the sub-menu when hovering over "user-change"
-  userChange.addEventListener('mouseenter', function () {
-    subMenu.style.display = 'block';
-  });
-
-  // Event listener for the logout button
-  var logoutButton = document.getElementById('logout');
-  logoutButton.addEventListener('click', function (e) {
-    e.preventDefault();
-    localStorage.removeItem('username');
-    userChange.textContent = 'Đăng nhập';
-    subMenu.style.display = 'none';
-  });
-}
-
 function userName() {
   var name = localStorage.getItem('username');
   var userChange = document.getElementById('user-change');
-  var subMenu = document.createElement('ul');
-  subMenu.classList.add('sub-menu');
-    subMenu.style.display = 'none'; // Initially hide the submenu
 
-  
+  // Check if the user is logged in
   if (name) {
-    userChange.textContent = name;
+    // Create a new <a> element with the updated content
+    var newA = document.createElement('a');
+    newA.id = 'user-change';
+    newA.classList.add('active');
+    newA.href = 'profile.html';
+    newA.textContent = name;
 
     // Create and populate the sub-menu
+    var subMenu = document.createElement('ul');
+    subMenu.classList.add('sub-menu');
+    subMenu.style.display = 'none'; // Initially hide the submenu
     subMenu.innerHTML = `
       <li><a href="/profile">Hồ sơ người dùng</a></li>
       <li><a href="#" id="logout">Đăng xuất</a></li>
     `;
 
+    // Add the new <a> element and sub-menu to the existing <li>
+    userChange.innerHTML = ''; // Clear the existing content
+    userChange.appendChild(newA);
     userChange.appendChild(subMenu);
 
     // Add event listener to hide the sub-menu when the cursor leaves "user-change"
@@ -73,11 +49,16 @@ function userName() {
     var logoutButton = document.getElementById('logout');
     logoutButton.addEventListener('click', function (e) {
       e.preventDefault();
-      localStorage.clear();
-      userChange.textContent = 'Đăng nhập';
-      subMenu.style.display = 'none';
+      localStorage.removeItem('username');
+      userName(); // Update the user interface
     });
   } else {
-    userChange.textContent = 'Đăng nhập';
+    // If the user is not logged in, reset the existing <li> content
+    userChange.innerHTML = '<a id="user-change" class="active" href="login.html">Đăng nhập</a>';
   }
 }
+
+// Call the userName function when the page loads
+document.addEventListener('DOMContentLoaded', function () {
+  userName();
+});
