b7ec2a9e28bcaeea5a45dfd44ce690cd3d5739dd
nchungojt07 <chung.nguyen-ojt07@devplus.edu.vn>
2023-11-14 10:30:41 +0700

    feat/EEW-9: management user success

---
+++ src/cloudiaryConfig.js
@@ -0,0 +1,11 @@
+// cloudinaryConfig.js
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+  cloud_name: 'darhyd9z6',
+  api_key: '231897141564684',
+  api_secret: 'BHuNu6feI_O2gGXeT0K2Cj3T5PU',
+  secure: true,
+});
+
+module.exports = cloudinary;
