f4bfca57f4565d8ab7d23fe4a3b4e96b71fbfbec
thaidq <thaidqgcd19963@fpt.edu.vn>
2023-11-17 14:09:04 +0700

    feat/EEW-5: Create Recommend Product

--- src/scripts/product-detail.js
+++ src/scripts/product-detail.js
@@ -33,3 +33,57 @@ fetch(`/api/products/${productId}?_expand=brand&_expand=category`)
     document.querySelector(".img").src = product.img;
   })
   .catch((error) => console.error("Error fetching product data: ", error));
+
+  //slide sản phẩm đề suất
+  async function fetchProductSale() {
+    try {
+      const response = await fetch(
+        "http://localhost:3000/api/products?sale=true"
+      );
+      if (!response.ok) {
+        throw new Error("Lỗi khi tải dữ liệu");
+      }
+      const products = await response.json();
+      await displaySlide(products);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function displaySlide(products) {
+    const categoryElement = document.querySelector(".product-category"); // Lấy phần tử có class .product-category
+  
+    if (categoryElement) {
+      const category = categoryElement.textContent.trim(); // Lấy giá trị của .product-category và loại bỏ khoảng trắng thừa
+  
+      document.querySelector("#your-class-first").innerHTML = products
+        .filter((product) => product.category === category) // Lọc sản phẩm dựa trên giá trị của .product-category
+        .map((product) => {
+          return `
+          <div id="slider" class="slider-content">
+            <a href="/product/${product.id}">
+              <img src=${product.img} alt=""/>
+            </a>
+            <div class="slide-info">
+              <div class="infor-text">
+                <a href="/product/${product.id}">
+                  <h6>${product.name}</h6>
+                </a>
+                <p>
+                  ${product.category}
+                </p>
+                <p class="text-amount">${product.price}.000đ</p>
+              </div>
+              <div class="infor-btn">
+                <button class="btn-add-card" onclick="haha(${product.id})" ><i class="fa-solid fa-cart-plus"></i></button>
+              </div>
+            </div>
+          </div>
+          `;
+        })
+        .join("");
+    }
+  }
+  
+
+  
