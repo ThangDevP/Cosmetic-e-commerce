1abb0cfb45684e873f444b2a7a177cadb52d30a8
tvpkhangojt07 <khang.truong-ojt07@devplus.edu.vn>
2023-11-17 10:10:21 +0700

    feat/EEW-8: change price currency

--- src/scripts/product-detail.js
+++ src/scripts/product-detail.js
@@ -8,16 +8,24 @@ fetch(`/api/products/${productId}`)
   .then((product) => {
     // Update the HTML elements with the product details
     document.querySelector(".product-title").textContent = product.name;
-    document.querySelector(".product-price-basic").textContent = product.price + " đ";
+    document.querySelector(".product-price-basic").textContent = product.price
+      .toLocaleString("vi-VN", { style: "currency", currency: "VND" })
+      .replace(/,/g, ".").replace(/₫/, "VNĐ");
     if (product.discount !== 0) {
-    document.querySelector(".product-price-discount").textContent =
-      (product.price * (100 - product.discount)) / 100 + " đ";
+      document.querySelector(".product-price-discount").textContent = (
+        (product.price * (100 - product.discount)) /
+        100
+      )
+        .toLocaleString("vi-VN", { style: "currency", currency: "VND" })
+        .replace(/,/g, ".").replace(/₫/, "VNĐ");
     } else {
       document.querySelector(".product-price-discount").textContent = "";
-      document.querySelector(".product-price-basic").style.textDecoration = 'none';
+      document.querySelector(".product-price-basic").style.textDecoration =
+        "none";
     }
 
-    document.querySelector(".product-description").textContent = product.description;
+    document.querySelector(".product-description").textContent =
+      product.description;
     document.querySelector(".product-category").textContent = product.category;
     document.querySelector(".img").src = product.img;
   })
