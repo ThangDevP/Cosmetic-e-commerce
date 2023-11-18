12ccf2f753aaf63fdc1330700e0d4cade48ae8d0
nchungojt07 <chung.nguyen-ojt07@devplus.edu.vn>
2023-11-18 23:02:36 +0700

    feat/EEW-38: fix showing product in dashboard

--- src/admin/dashboard/dashboard.js
+++ src/admin/dashboard/dashboard.js
@@ -1,39 +1,45 @@
 
 document.addEventListener("DOMContentLoaded", fetchDataAndPopulateTable());
-function fetchDataAndPopulateTable() {
-fetch(`http://localhost:3000/api/orders?_expand=user`)
-    .then((response) => response.json())
-    .then((data) => {
-        let totalRevenue = 0;
-        ordersTableBody = document.getElementById("ordersTableBody");
-        ordersTableBody.innerHTML = "";
-
-    data.forEach((order) => {
+async function fetchDataAndPopulateTable() {
+    try {
+      const response = await fetch('http://localhost:3000/api/orders?_expand=user');
+      const data = await response.json();
+  
+      const ordersTableBody = document.getElementById('ordersTableBody');
+      const totalRevenueElement = document.getElementById('totalRevenue');
+      let totalRevenue = 0;
+  
+      for (const order of data) {
         const row = ordersTableBody.insertRow();
         const idCell = row.insertCell(0);
         const customerCell = row.insertCell(1);
         const inforCell = row.insertCell(2);
         const itemsCell = row.insertCell(3);
         const totalCell = row.insertCell(4);
-
+  
         idCell.textContent = order.id;
         customerCell.innerHTML = `${order.user.username} <br> (${order.user.email})`;
-        inforCell.innerHTML = `${order.customerInfo.username} (${order.customerInfo.phoneNumber}) <br> ${order.customerInfo.addr} , ${order.customerInfo.ward} , ${order.customerInfo.district} , ${order.customerInfo.city}`;
-        if (order.itemsDetails && order.itemsDetails.length > 0) {
-            itemsCell.innerHTML = order.itemsDetails.map(item => `${item.quantity} ${item.productName}`).join('<br>');
-        } else {
-            itemsCell.textContent = 'No items';
+        inforCell.innerHTML = `${order.customerInfo.username} (${order.customerInfo.phoneNumber}) <br> ${order.customerInfo.addr}, ${order.customerInfo.ward}, ${order.customerInfo.district}, ${order.customerInfo.city}`;
+  
+        let itemsDetailsHtml = '';
+        for (const item of order.itemsDetails) {
+          // Fetch product details for each item
+          const productResponse = await fetch(`http://localhost:3000/api/products/${item.productId}`);
+          const productData = await productResponse.json();
+  
+          itemsDetailsHtml += `${item.quantity} ${productData.productName} <br>`;
         }
+  
+        itemsCell.innerHTML = itemsDetailsHtml || 'No items';
         totalCell.textContent = order.total;
         totalRevenue += order.total;
-
-    });
-    document.getElementById("totalRevenue").textContent = totalRevenue;
-    })
-    .catch((error) => {
-    console.error("Lỗi khi lấy dữ liệu từ API: ", error);
-    });
-}
+      }
+  
+      totalRevenueElement.textContent = totalRevenue;
+    } catch (error) {
+      console.error('Lỗi khi lấy dữ liệu từ API:', error);
+    }
+  }
 function logoutUser() {
     const confirmLogout = confirm("Are you sure you want to logout?");
         if (confirmLogout) {
